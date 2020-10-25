b4f0390e87a05e7f11c88b67fb897436395c708b
loxt <emannuelloxt@hotmail.com>
2020-10-25 13:26:08 -0400

    test: ensure call the create user function

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 
+const mockUser = {
+  email: 'contrataeulogo@gmail.com',
+  firstName: 'Contrata o',
+  id: '777',
+  lastName: 'Loxt',
+  password: '123456',
+  confirmPassword: '123456',
+  phone: '996944782',
+};
+
+const mockUserRepository = () => ({
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+  create: jest.fn(),
+});
+
 describe('UsersService', () => {
-  let service: UsersService;
+  let userService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useFactory: mockUserRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = await module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  it('should be call the create user function', async () => {
+    await userService.create.mockReturnValue(mockUser);
+
+    await expect(userService.create).not.toHaveBeenCalled();
+
+    userService.create(mockUser);
+    expect(userService.create).toHaveBeenCalled();
+
+    expect(userService.create).toHaveBeenCalledWith(mockUser);
   });
 });

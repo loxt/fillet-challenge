55fc5ea169a14abd2bc4e3cc49d929c6e18e0340
loxt <emannuelloxt@hotmail.com>
2020-10-25 00:20:42 -0400

    fix: resolver decorator and nullable query

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -1,12 +1,13 @@
-import { Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Query, Mutation, Args, Int, Resolver } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 
+@Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
-  @Mutation(() => User)
+  @Mutation(() => User, { nullable: true })
   async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
     return this.usersService.create(createUserInput);
   }
